a74339d2e9e196ffc11868ce339d67f14be913f9
Manula <manula.nxt2@gmail.com>
2021-11-29 23:04:23 +0530

    link list

--- link_list/ex1.cpp
+++ link_list/ex1.cpp
@@ -2,13 +2,46 @@
 
 using namespace std;
 
-int main()
-{
-
-}
+Node* head= NULL;
 
 class Node{
     public:
     int val;
     Node* Next;
 };
+
+class LinkList{
+    
+};
+
+void print(Node* head){
+    Node* temp=head;
+
+    while (temp!=NULL)
+    {
+        cout<<temp->val<<endl;
+        temp=temp->Next;
+    }
+    
+}
+
+void appendnode(int val){
+
+}
+int main()
+{
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    head->val=20;
+    second->val=50;
+    third->val=60;
+
+    head->Next = second;
+    second->Next=third;
+    third->Next=NULL;
+
+    print(head);
+}
+
